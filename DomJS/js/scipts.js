d89459c72f02d20bca10de01bfcaeb41f3227b36
yaiv <yaiv.gm@gmail.com>
2025-04-09 13:11:52 -0600

    Se refactorizo envio formulario mediante funcion

--- DomJS/js/scipts.js
+++ DomJS/js/scipts.js
@@ -11,8 +11,8 @@
 
 //querySelectorAll 
 
-const enlaces = document.querySelectorAll('.navegacion a');
-console.log(enlaces)
+// const enlaces = document.querySelectorAll('.navegacion a');
+// console.log(enlaces)
 
 //Va a retornar todos los que concuerden con ese selector especificado 
 // Para acceder a alguno en especifico es como con los arreglos ej: [0]
@@ -22,7 +22,7 @@ console.log(enlaces)
 //enlaces[0].classList.add('nueva-clase');  Se genera nueva clase 
 //enlaces[0].classList.remove('navegacion__enlace'); Se elimina clase 
 
-8
+
 //getElementBy 
 
 const heading2 = document.getElementById('heading');
@@ -80,25 +80,28 @@ nuevoEnlace.classList.add('navegacion__enlace')
 // }
 
 
+
+
 //Seleccionar elementos y asociarles eventos 
+//ESTA ASOCIADO A CUALQUIER ELEMENTO, NO COMO EN submit que esta para el formulario
 
-const btnEnviar = document.querySelector('.boton--primario');
-btnEnviar.addEventListener('click', function(evento) {
-    console.log(evento);
-    evento.preventDefault();
-    console.log('enviando formulario')
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', function(evento) {
+//     console.log(evento);
+//     evento.preventDefault();
+//     console.log('enviando formulario')
 
-});
+// });
 
 //Eventos de los inputs y textarea
 
 //Se genera objeto de datos de los formularios 
 
-const datos = {
-    nombre: '',
-    email: '',
-    mensaje: ''
-}
+// const datos = {
+//     nombre: '',
+//     email: '',
+//     mensaje: ''
+// }
 
 //AL USAR QUERYSELECTOR SE TIENE DISPONIBLE ADDEVENTLISTER 
 // const nombre = document.querySelector('#nombre');
@@ -120,26 +123,253 @@ const datos = {
 
 //tambien se puede realizar a traves de una funcion para simplificar codigo 
 
+// const nombre = document.querySelector('#nombre');
+// const email = document.querySelector('#email');
+// const mensaje = document.querySelector('#mensaje');
+
+// nombre.addEventListener('input', leerTexto);
+// email.addEventListener('input', leerTexto);
+// mensaje.addEventListener('input', leerTexto);
+
+// // function leerTexto () {
+// //     console.log('EscribiendoTexto')
+// // }
+
+// //Para pasar el evento solo se hace con la letra e en la funcion creada 
+// function leerTexto (e) {
+//     // console.log(e.target.value);
+
+//     datos[e.target.id] = e.target.value;
+
+//     console.log(e.target);
+//     console.log(datos);
+// }
+
+// //Eventos de click y eventos de input 
+
+// //EVENTOS EN EL FORMULARIO 
+
+// //EVENTO submit   para formularios 
+// const formulario = document.querySelector('.formulario')
+// formulario.addEventListener('submit', function(evento) {
+//     evento.preventDefault();
+//     console.log('Enviando Formularios')
+// })
+
+
+// Como organizar el codigo JS 
+// variables en la parte superior 
+// despues todos los addEventListener
+// Y luego las funciones
+
+
+//Ejemplo correcto 
+
+// const datos = {
+//     nombre: '',
+//     email: '',
+//     mensaje: ''
+// }
+
+// const nombre = document.querySelector('#nombre');
+// const email = document.querySelector('#email');
+// const mensaje = document.querySelector('#mensaje');
+// const formulario = document.querySelector('.formulario');
+
+// nombre.addEventListener('input', leerTexto);
+// email.addEventListener('input', leerTexto);
+// mensaje.addEventListener('input', leerTexto);
+
+// //El evento de submit 
+// formulario.addEventListener('submit', function(evento) {
+//     evento.preventDefault();
+// });
+
+// //Se valida el formulario 
+
+
+// //Enviar el formulario 
+
+//     function leerTexto (e) {
+//         // console.log(e.target.value);
+    
+//         datos[e.target.id] = e.target.value;
+    
+//         console.log(datos);
+//     }
+
+
+
+// //Se genera validador de formulario
+
+// //Este es un objeto global
+// const datos = {
+//     nombre: '',
+//     email: '',
+//     mensaje: ''
+// }
+
+// const nombre = document.querySelector('#nombre');
+// const email = document.querySelector('#email');
+// const mensaje = document.querySelector('#mensaje');
+// const formulario = document.querySelector('.formulario');
+
+// nombre.addEventListener('input', leerTexto);
+// email.addEventListener('input', leerTexto);
+// mensaje.addEventListener('input', leerTexto);
+
+// //El evento de submit 
+// formulario.addEventListener('submit', function(evento) {
+//     evento.preventDefault();
+
+
+//     //Se valida el formulario 
+
+//     //Se van a extraer los valores del objeto global 
+//     const { nombre, email, mensaje } = datos;
+
+//     console.log(nombre);
+//     console.log(email);
+//     console.log(mensaje);
+
+//     if ( nombre === '' || email === '' || mensaje === '') {
+//         // console.log('Todos los campos son obligatorios');   SE GENERA FUNCION, ya que nadie revisaria la consola de esta manera 
+
+//         mostrarError ('Todos los campos son obligatorios funcion');
+//         return; //Corta la ejecucion del codigo si se cumple la condicion que se tiene en el if
+//     }
+
+
+//     //Se crea alerta de Enviar Formulario correctamente 
+//     MostrarMensaje('Formulario enviado correctamente fun');
+// });
+
+
+
+//     function leerTexto (e) {
+//         // console.log(e.target.value);
+    
+//         datos[e.target.id] = e.target.value;
+    
+//         // console.log(datos);
+//     }
+
+// //Muestra alerta de envio correcto de formulario 
+
+// function MostrarMensaje (mensaje) {
+//     const alerta = document.createElement('P');
+//     alerta.textContent = mensaje
+
+//     //Se genera clase para css 
+//     alerta.classList.add('correcto');
+
+//     //Se agrega al documento
+//     formulario.appendChild ( alerta );
+
+//     //Desaparezca despues de 5 segundos 
+//     setTimeout (() =>{
+//         alerta.remove();
+//     }, 5000);
+// }
+
+
+// //Muestra error en pantalla por llenado de campos 
+
+// function mostrarError (mensaje) {
+//     const error = document.createElement('P');
+//     error.textContent = mensaje
+
+//     //Se le agrega clase css
+//     error.classList.add('error');
+
+//     //Se agrega al documento
+//     formulario.appendChild ( error );
+
+//     //Desaparezca despues de 5 segundos 
+//     setTimeout (() =>{
+//         error.remove();
+//     }, 5000);
+
+// }
+
+//Replicar alerta para reforzar conocimiento 
+
+
+
+//REFACTORING DE CODIGO se realiza una vez que el codigo ya funciona, se trata de optimizarlo pero entendiendo lo que se hace  
+
+
+
+//Este es un objeto global
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
 const nombre = document.querySelector('#nombre');
 const email = document.querySelector('#email');
 const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
 
 nombre.addEventListener('input', leerTexto);
 email.addEventListener('input', leerTexto);
 mensaje.addEventListener('input', leerTexto);
 
-// function leerTexto () {
-//     console.log('EscribiendoTexto')
-// }
+//El evento de submit 
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
 
-//Para pasar el evento solo se hace con la letra e en la funcion creada 
-function leerTexto (e) {
-    // console.log(e.target.value);
 
-    datos[e.target.id] = e.target.value;
+    //Se valida el formulario 
 
-    console.log(e.target);
-    console.log(datos);
+    //Se van a extraer los valores del objeto global 
+    const { nombre, email, mensaje } = datos;
+
+    console.log(nombre);
+    console.log(email);
+    console.log(mensaje);
+
+    if ( nombre === '' || email === '' || mensaje === '') {
+        // console.log('Todos los campos son obligatorios');   SE GENERA FUNCION, ya que nadie revisaria la consola de esta manera 
+
+        mostrarAlerta ('Todos los campos son obligatorios fun', true);
+        return; //Corta la ejecucion del codigo si se cumple la condicion que se tiene en el if
+    }
+
+
+    //Se crea alerta de Enviar Formulario correctamente 
+    mostrarAlerta('Formulario enviado correctamente fun');
+});
+
+
+
+    function leerTexto (e) {
+        // console.log(e.target.value);
+    
+        datos[e.target.id] = e.target.value;
+    
+        // console.log(datos);
+    }
+
+
+//Se genera funcion refactoring 
+
+function mostrarAlerta (mensaje, error = null ) {
+    const alerta = document.createElement('P');
+    alerta.textContent  = mensaje;
+
+    if (error) {
+        alerta.classList.add('error');
+    }else {
+        alerta.classList.add('correcto');
+    }
+
+    formulario.appendChild(alerta);
+
+    //Desaparezca despues de 5 segundos 
+    setTimeout (() =>{
+         alerta.remove();
+    }, 5000);
 }
 
-//Eventos de click y eventos de input 
